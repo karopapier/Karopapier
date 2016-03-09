c098761026506912ccb2a296bc62b023d5deb878
KaroDidi <didi@karopapier.de>
2016-03-10 00:50:55 +0100

    integrated complete sf3 + gamepreview

---
+++ README.md
@@ -0,0 +1,4 @@
+wrapper2
+========
+
+A Symfony project created on March 9, 2016, 10:06 pm.
---
+++ src/Karopapier/Karopapier.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Karopapier;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Karopapier extends Bundle
+{
+}
