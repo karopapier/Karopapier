62cfd1cf612d00eb85c783f36ae692eefcdfb6a7
KaroDidi <didi@karopapier.de>
2016-06-19 00:29:20 +0200

    Start API login

--- src/AppBundle/Controller/Api/UserController.php
+++ src/AppBundle/Controller/Api/UserController.php
@@ -10,7 +10,9 @@ namespace AppBundle\Controller\Api;
 
 use AppBundle\Entity\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class UserController extends AbstractApiController
 {
@@ -30,5 +32,21 @@ class UserController extends AbstractApiController
     public function showSlugAction(User $user)
     {
         return new JsonResponse($user->toArray());
+    }
+
+    /**
+     * @Route("/user/login", name="api_user_login")
+     * @Method("POST")
+     */
+    public function loginAction($username, $password)
+    {
+        $json = $request->getContent();
+        $obj = json_decode($json, true);
+        if (isset($obj["login"]) && isset($obj["password"])) {
+            $login = $obj["login"];
+            $password = $obj["password"];
+        } else {
+            throw new AccessDeniedHttpException();
+        }
     }
 }
