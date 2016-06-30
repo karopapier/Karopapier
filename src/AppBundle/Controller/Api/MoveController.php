93f06cd5efea3c36d20710317b3497d683fae2c3
KaroDidi <didi@karopapier.de>
2016-06-30 23:48:23 +0200

    Usermap

---
+++ src/AppBundle/Controller/Api/MoveController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pdietrich
+ * Date: 30.06.2016
+ * Time: 13:39
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Component\BrowserKit\Request;
+
+class MoveController
+{
+
+    /**
+     * @Route("/user/new/{id}", name="user_show")
+     */
+    public function addAction(Request $request, Game $game)
+    {
+        
+        
+    }
+
+}
