4aa08f840681e82e06386ea7fd4bab6f06fc8d0d
KaroDidi <didi@karopapier.de>
2016-06-30 23:48:58 +0200

    nelmio/cors

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,18 +8,17 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
-                new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-                new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-                new Symfony\Bundle\TwigBundle\TwigBundle(),
-                new Symfony\Bundle\MonologBundle\MonologBundle(),
-                //new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-                new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-                new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-                new AppBundle\AppBundle(),
-                new Karopapier\Karopapier(),
-                new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-
-
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            //new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new AppBundle\AppBundle(),
+            new Karopapier\Karopapier(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/AppBundle/Controller/Api/MoveController.php
+++ src/AppBundle/Controller/Api/MoveController.php
@@ -6,21 +6,118 @@
  * Time: 13:39
  */
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Api;
 
-
-use Symfony\Component\BrowserKit\Request;
+use AppBundle\Entity\Game;
+use AppBundle\Entity\Move;
+use AppBundle\Model\Motion;
+use AppBundle\Model\Position;
+use AppBundle\Model\Vector;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class MoveController
 {
 
     /**
-     * @Route("/user/new/{id}", name="user_show")
+     * @Route("/game/{id}/move", name="api_move_add", requirements={"id": "\d+"})
+     * @Security("has_role('ROLE_USER')")
+     * @Method("POST")
+     * @param Game $game
      */
     public function addAction(Request $request, Game $game)
     {
-        
-        
-    }
+        throw  new AccessDeniedHttpException;
+        //retrieve request body
+        $jsonString = $request->getContent();
+        $json = json_decode($jsonString, true);
 
+        $x = $json['x'];
+        $y = $json['y'];
+        $xv = $json['xv'];
+        $yv = $json['yv'];
+
+        $user = $this->getUser();
+
+        $move = new Move();
+        $move->setMotion(
+            new Motion(
+                new Position($x, $y),
+                new Vector($xv, $yv)
+            )
+        );
+        $move->setUser($user);
+        $move->setGame($game);
+
+        $dm = $this->get("doctrine")->getManager();
+        $dm->persist($move);
+        $dm->flush();
+
+        /*
+        CORS
+        
+        usersession
+        
+        parameter prüfen
+        GID, xpos, ypos, xvec, yvec, startx, starty, movemessage
+        
+        bei startx/y prüfen, ob nicht schon gezoge (Warum separat behandeln??)
+        
+        
+        
+        TRANSACTION!!!!
+        game sperren (select for update)
+        
+        prüfen, ob xvec/yvec änderung in x/y <=1 
+        
+        Prüfen, ob vec und pos zueinander passen
+        
+        Prüfen, ob zug möglich
+        
+        
+        Bei Startpunkt prüfen, ob es wirklich Startpunkte sind
+        und nicht schon besetzt
+        
+        Zug ins logfile
+        
+        Prüfen, welche Checkpoints überfahren werden
+        
+        Zug in movestable
+        ggf message reinschreiben
+        ggf mit CP info
+        
+        
+        Prüfen, ob ins Ziel gezogen wird und alle Checkpoins gecheckt sind
+        
+        Bei Zielzug
+        - Position errechnen
+        - einen Parc-Fermée Zug hinterher
+        - log
+        - finished setzen
+        - Realtime info über beendet
+        - Streckenrekord prüfen
+        
+        Sofern noch spieler übrig
+        NEXTONE
+        (ggf mit neuer runde)
+        sonst spiel beendet markieren
+        
+        COMMIT
+        
+        Real time für Zug an alle
+        Real time für user dran dec
+        Real time für next one dran inc
+        real time game finished?
+        
+        Prüfen ob next one eine email will? Senden
+        
+        Thumbnail update
+        Wollust update
+        
+        Ausgabe,wie's weiter geht
+        */
+    }
 }
