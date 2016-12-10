a5f7f5b6f17b16577394608906ff293595c10046
KaroDidi <didi@karopapier.de>
2016-12-10 22:47:52 +0100

    Smilifier Test

---
+++ tests/AppBundle/Services/SmilifierTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: monti
+ * Date: 10.12.2016
+ * Time: 22:37
+ */
+
+namespace Tests\AppBundle\Services;
+
+
+use AppBundle\Services\Smilifier;
+
+
+class SmilifierTest extends \PHPUnit_Framework_TestCase
+{
+
+}
