cddaa7ba1d82b0fc16a49787edb1cce250a27fb6
KaroDidi <didi@karopapier.de>
2016-12-11 11:27:22 +0100

    guessRaw for chat lines

--- src/AppBundle/Services/Smilifier.php
+++ src/AppBundle/Services/Smilifier.php
@@ -26,12 +26,16 @@ class Smilifier
 
     public function smilify($content)
     {
+        /*
         $content = str_replace('<', '&lt;', $content);
         $content = str_replace('>', '&gt;', $content);
         $content = str_replace('\"', '&quot;', $content);
         $content = str_replace("\\\'", "'", $content);
         $content = str_replace('"', '&quot;', $content);
         $content = str_replace("\n", '<BR>', $content);
+        */
+
+        $content = htmlentities($content);
 
         // if there are two : at all...
         if (preg_match('/:.*:/', $content)) {
@@ -70,5 +74,37 @@ class Smilifier
             $content = $content . array_pop($textparts);
         }
         return $content;
+    }
+
+    /**
+     * Try to guess the original "raw" string that the user entered that was converted to the given string
+     * @param string $text
+     * @return string
+     */
+    public function guessRaw($text)
+    {
+        $raw = $text;
+        $raw = str_replace('<BR>', '', $raw);
+
+        //find links
+        $raw = preg_replace('/<a href="(.*?)">(.*?)<\/a>/', '-:Link text=\\2 url=\\1 Link:-', $raw);
+
+        //find pics
+        $raw = preg_replace('/<img src="(.*?)" \/>/', '-:Pic src=\\1 Pic:-', $raw);
+
+        //find F, K, RED
+        $raw = str_ireplace("<B>", "-:F", $raw);
+        $raw = str_ireplace("</B>", "F:-", $raw);
+        $raw = str_ireplace("<I>", "-:K", $raw);
+        $raw = str_ireplace("</I>", "K:-", $raw);
+        $raw = preg_replace('/<font color=#CC0000>/i', '-:RED', $raw);
+        $raw = preg_replace('/<\/font>/i', 'RED:-', $raw);
+
+        //find smiley
+        $raw = preg_replace('/<img src="\/images\/smilies\/(.*?).gif" alt="(.*?)" title="(.*?)">/', ':\\1:', $raw);
+        $raw = preg_replace('/<IMG SRC="bilder\/smilies\/(.*?).gif" alt="(.*?)" title="(.*?)">/', ':\\1:', $raw);
+
+        //$raw = preg_replace("/-:Pic src=(.*) Pic:-/", "<img src=\"\\1\" />", $value);
+        return trim($raw);
     }
 }
--- tests/AppBundle/Services/SmilifierTest.php
+++ tests/AppBundle/Services/SmilifierTest.php
@@ -26,7 +26,10 @@ class SmilifierTest extends \PHPUnit_Framework_TestCase
         $smileyHolder->expects($this->any())
                 ->method('getSmilies')
                 ->will($this->returnValue(array(
-                        "approve"
+                        "approve",
+                        "tongue",
+                        "devilfire",
+                        "unused"
                 )));
         $this->smilifier = new Smilifier($smileyHolder, $logger);
     }
@@ -39,5 +42,63 @@ class SmilifierTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('Hallo <i>KURSIV</i>', $sm->smilify('Hallo -:KKURSIVK:-'), 'Kursiv');
         $this->assertEquals('Hallo <img src="/images/smilies/approve.gif" alt="approve" title="approve">', $sm->smilify('Hallo :approve:'), 'smiley to img');
         $this->assertEquals('Hallo :ismiregal: gibts nich', $sm->smilify('Hallo :ismiregal: gibts nich'), 'no change if smiley does not exist');
+
+        $res = $sm->smilify(':tongue:');
+        $exp = '<img src="/images/smilies/tongue.gif" alt="tongue" title="tongue">';
+        $this->assertEquals($exp, $res, 'converts smilies');
+
+        $res = $sm->smilify('http://');
+        $exp = 'http://';
+        $this->assertEquals($exp, $res, 'leaves unmatched stuff unchanged');
+
+        $res = $sm->smilify('Hier ist ein -:Link text=beididi url=http://www.beididi.de Link:- im Text');
+        $exp = 'Hier ist ein <a href="http://www.beididi.de">beididi</a> im Text';
+        $this->assertEquals($exp, $res, 'converts links');
+
+        $res = $sm->smilify('-:Pic src=http://www.brot.de/logo.gif Pic:- und -:Pic src=http://www.brot.de/logo.png Pic:- sind Bilder');
+        $exp = '<img src="http://www.brot.de/logo.gif" /> und <img src="http://www.brot.de/logo.png" /> sind Bilder';
+        $this->assertEquals($exp, $res, 'converts pics');
+
+        $res = $sm->smilify('');
+        $exp = '';
+        $this->assertEquals($exp, $res, 'NULL is NULL');
+    }
+
+    public function testEscaping()
+    {
+        $sm = $this->smilifier;
+        $this->assertEquals('&lt;SCRIPT&gt;', $sm->smilify("<SCRIPT>"), 'escape tags');
+        $this->assertEquals('Wie schaut\'s aus? &quot;FETT?&quot;', $sm->smilify('Wie schaut\'s aus? "FETT?"'), 'escape tags');
+    }
+
+    public function testGuessRaw()
+    {
+        $sm = $this->smilifier;
+
+        $line1 = '<IMG SRC="bilder/smilies/devilfire.gif" alt="devilfire" title="devilfire"> <font color=#CC0000> MUAAAHAHAHAHAHAHA </font> <IMG SRC="bilder/smilies/devilfire.gif" alt="devilfire" title="devilfire"> <BR>';
+        $line2 = '<IMG SRC="bilder/smilies/devilfire.gif" alt="devilfire" title="devilfire"> <font color=#CC0000> <b> MUAAAAAHAHAHAHAHAH </b> </font> <IMG SRC="bilder/smilies/devilfire.gif" alt="devilfire" title="devilfire"> <BR>';
+        $raw1 = ':devilfire: -:RED MUAAAHAHAHAHAHAHA RED:- :devilfire:';
+        $raw2 = ':devilfire: -:RED -:F MUAAAAAHAHAHAHAHAH F:- RED:- :devilfire:';
+
+        $this->assertEquals($raw1, $sm->guessRaw($line1), "guess raw of devils MUHAHA");
+        $this->assertEquals($raw2, $sm->guessRaw($line2), "guess raw of other devils MUHAHA");
+
+        $pic1 = '<img src="http://smile.welcomes-you.com/Unhappy/finger-020.gif" />';
+        $rawpic1 = '-:Pic src=http://smile.welcomes-you.com/Unhappy/finger-020.gif Pic:-';
+        $this->assertEquals($rawpic1, $sm->guessRaw($pic1), "guess raw of pic");
+
+        $link1 = '<a href="http://www.karopapier.de/showmap.php?GID=94201">Direktlink_1.0</a>';
+        $rawlink1 = '-:Link text=Direktlink_1.0 url=http://www.karopapier.de/showmap.php?GID=94201 Link:-';
+        $this->assertEquals($rawlink1, $sm->guessRaw($link1), "guess raw of link");
+    }
+
+    public function testFullcircle()
+    {
+        /* Test if smilified guess is similar to original */
+        $sm = $this->smilifier;
+
+        $line = '<IMG SRC="bilder/smilies/devilfire.gif" alt="devilfire" title="devilfire"> <font color=#CC0000> MUAAAHAHAHAHAHAHA </font> <IMG SRC="bilder/smilies/devilfire.gif" alt="devilfire" title="devilfire">';
+        $after = '<IMG SRC="/images/smilies/devilfire.gif" alt="devilfire" title="devilfire"> <font color=#CC0000> MUAAAHAHAHAHAHAHA </font> <IMG SRC="/images/smilies/devilfire.gif" alt="devilfire" title="devilfire">';
+        $this->assertContains($after, $sm->smilify($sm->guessRaw($line)), 'guess + smilify = result', true);
     }
 }
