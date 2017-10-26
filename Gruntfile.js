066a6db36cd1533b115abf58b2ef07a2b1888cbe
Peter Dietrich <xosofox@gmail.com>
2017-10-26 13:37:18 +0200

    Make Gruntfile check for key/cert

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,16 @@ module.exports = function(grunt) {
     // Project configuration.
     require('time-grunt')(grunt);
 
+    var keypath = "/etc/ssl/panamapapier/privkey.pem";
+    var certpath = "/etc/ssl/panamapapier/cert.pem";
     var livereloadConfig = {
         port: 20001,
-        key: grunt.file.read("/etc/ssl/panamapapier/privkey.pem"),
-        cert: grunt.file.read("/etc/ssl/panamapapier/cert.pem"),
     };
+
+    if (grunt.file.exists(keypath, certpath)) {
+        livereloadConfig["key"] = grunt.file.read(keypath);
+        livereloadConfig["cert"] = grunt.file.read(certpath);
+    }
 
     var cleanCssOptions = {
         level: 2
