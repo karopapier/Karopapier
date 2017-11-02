3f22d3d016ec9dc147b0e28137c616e9274602be
Peter Dietrich <xosofox@gmail.com>
2017-11-02 23:56:30 +0100

    NewUser DTO + Login Assertion

--- src/AppBundle/DTO/NewUser.php
+++ src/AppBundle/DTO/NewUser.php
@@ -8,8 +8,19 @@
 
 namespace AppBundle\DTO;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 class NewUser
 {
+    /**
+     * @Assert\Regex("/^[A-Za-z0-9-_\[\]\(\)ÄÖÜäöüß;=?\.@ ]{3,20}$/")
+     * @var string
+     */
+    private $login;
 
+    public function __construct($login)
+    {
+        $this->login = $login;
+    }
 }
--- tests/AppBundle/DTO/NewUserTest.php
+++ tests/AppBundle/DTO/NewUserTest.php
@@ -6,11 +6,65 @@
  * Time: 23:32
  */
 
-namespace NewUserTest;
+namespace AppBundle\Tests;
 
 use AppBundle\DTO\NewUser;
+use Symfony\Component\Validator\Validation;
 
 class NewUserTest extends \PHPUnit_Framework_TestCase
 {
+
+    public function testLoginAssertion()
+    {
+        $usernames = array(
+            'תהו' => false,
+            '-D0G-' => true,
+            '0n3w0rld' => true,
+            '100wasser' => true,
+            '1malDidiaergern' => true,
+            ';Philipp' => true,
+            '==PartyA' => true,
+            '=quiEtscHEEntch' => true,
+            '????' => true,
+            'a.lo' => true,
+            'Automatikk2' => true,
+            'BassBoostinator' => true,
+            'Bl4ckfir3' => true,
+            'Bl@ckSp@rk' => true,
+            'chris.Ytterbium' => true,
+            'CoCa Mike' => true,
+            'Dahollända' => true,
+            'Daniel____' => true,
+            'dE KuYp3r' => true,
+            'Der heilige Geis' => true,
+            'Ein Dude kommt selte' => true,
+            'HansJoachimloewenzah' => true,
+            'Kindergärtnerin' => true,
+            'liebergehenalsfahren' => true,
+            'ma(V3)xx' => true,
+            'Müllauto' => true,
+            'März-edes-CLK' => true,
+            'NICHT EINLADEN' => true,
+            'Schüler05' => true,
+            'ÜmläütT€ßtÜsér' => false,
+            '[lunatic]suicide' => true,
+            'äöüßÄÖÜ' => true,
+            'ödipups' => true,
+            'Örni' => true,
+            '_ESKAL' => true,
+            'Derwodernameisteinfachvielzulang' => false,
+            'zk' => false,
+        );
+
+        $validator = Validation::createValidatorBuilder()->enableAnnotationMapping()->getValidator();
+
+        foreach ($usernames as $username => $expectedAsValid) {
+            $u = new NewUser($username);
+            $errorsExpected = $expectedAsValid ? 0 : 1;
+
+            $errors = $validator->validate($u);
+            $this->assertEquals($errorsExpected, count($errors), "Name ".$username);
+        }
+    }
 
 }
