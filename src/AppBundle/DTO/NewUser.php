7a270122415a00da5dcc1119f76a6665139edb04
Peter Dietrich <xosofox@gmail.com>
2017-11-02 23:55:33 +0100

    Greenify unit tests

---
+++ src/AppBundle/DTO/NewUser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: monti
+ * Date: 02.11.2017
+ * Time: 23:28
+ */
+
+namespace AppBundle\DTO;
+
+
+class NewUser
+{
+
+}
---
+++ tests/AppBundle/DTO/NewUserTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: monti
+ * Date: 02.11.2017
+ * Time: 23:32
+ */
+
+namespace NewUserTest;
+
+use AppBundle\DTO\NewUser;
+
+class NewUserTest extends \PHPUnit_Framework_TestCase
+{
+
+}
