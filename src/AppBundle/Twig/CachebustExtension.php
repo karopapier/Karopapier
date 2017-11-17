2938443670170bdcaa2ec0e32f295114cb2aac49
Peter Dietrich <xosofox@gmail.com>
2017-11-17 10:22:52 +0100

    CachebustExtension

---
+++ src/AppBundle/Twig/CachebustExtension.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pdietrich
+ * Date: 18.07.2016
+ * Time: 23:57
+ */
+
+namespace AppBundle\Twig;
+
+
+class CachebustExtension extends \Twig_Extension
+{
+    public function __construct($webpath)
+    {
+        $this->manifest = json_decode(file_get_contents($webpath.'/cachebust.json'), true);
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'getCachebustPath' => new \Twig_SimpleFunction("cachebust", array($this, 'getCachebustPath')),
+        );
+    }
+
+    public function getCachebustPath($file)
+    {
+        if (isset($this->manifest[$file])) {
+            return $this->manifest[$file];
+        }
+
+        return $$file;
+    }
+}
