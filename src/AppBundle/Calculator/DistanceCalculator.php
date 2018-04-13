7d5c01dcaed327916e065c88ffc51352d3136b93
Peter Dietrich <xosofox@gmail.com>
2018-04-13 19:35:06 +0200

    AUTOWIRE & DEPRECATIONS Calculator

--- src/AppBundle/Calculator/DistanceCalculator.php
+++ src/AppBundle/Calculator/DistanceCalculator.php
@@ -6,19 +6,19 @@
  * Time: 19:03
  */
 
-namespace AppBundle\Services\User;
+namespace AppBundle\Calculator;
 
 
 use AppBundle\Entity\User;
-use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class DistanceCalculator
 {
 
-    /** @var EntityManager */
+    /** @var ObjectManager */
     private $em;
 
-    public function __construct(EntityManager $em)
+    public function __construct(ObjectManager $em)
     {
         $this->em = $em;
     }
@@ -31,7 +31,9 @@ class DistanceCalculator
         //Driven milimeters
         //SELECT SUM( SQRT( POW( x_vec, 2 ) + POW( y_vec, 2 ) ) * 5 ) from karo_moves where U_ID=1
         $connection = $em->getConnection();
-        $statement = $connection->prepare("SELECT SUM( SQRT( POW( x_vec, 2 ) + POW( y_vec, 2 ) ) * 5 ) as distance from karo_moves where U_ID=:id");
+        $statement = $connection->prepare(
+            "SELECT SUM( SQRT( POW( x_vec, 2 ) + POW( y_vec, 2 ) ) * 5 ) AS distance FROM karo_moves WHERE U_ID=:id"
+        );
         $statement->bindValue('id', $id);
         $statement->execute();
         $results = $statement->fetchAll();
@@ -39,6 +41,7 @@ class DistanceCalculator
         if ((count($results)) > 0) {
             return $results[0]["distance"];
         }
+
         return 0;
     }
 
@@ -46,6 +49,7 @@ class DistanceCalculator
     {
         $distance = $this->calculateDistance($user);
         $this->setDistance($user, $distance);
+
         return $distance;
     }
 
