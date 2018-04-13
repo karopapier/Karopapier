f93b90b143fb68dbd3f30c337fb98214f6688154
Peter Dietrich <xosofox@gmail.com>
2018-04-13 18:46:27 +0200

    AUTOWIRE & DEPRECATIONS VisitTracker

--- src/AppBundle/Tracker/VisitTracker.php
+++ src/AppBundle/Tracker/VisitTracker.php
@@ -6,21 +6,30 @@
  * Time: 00:40
  */
 
-namespace AppBundle\Services;
+namespace AppBundle\Tracker;
 
 use AppBundle\Entity\User;
-use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Persistence\ObjectManager;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\AuthenticationEvents;
 use Symfony\Component\Security\Core\Event\AuthenticationEvent;
 
-class VisitLogService
+class VisitTracker implements EventSubscriberInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+    /**
+     * @var ObjectManager
+     */
     private $em;
 
-    public function __construct(\Psr\Log\LoggerInterface $logger, EntityManager $em)
+    public function __construct(LoggerInterface $logger, ObjectManager $em)
     {
-        $this->em = $em;
         $this->logger = $logger;
+        $this->em = $em;
     }
 
     public function onSecurityAuthenticationSuccess(AuthenticationEvent $e)
@@ -28,10 +37,12 @@ class VisitLogService
         $token = $e->getAuthenticationToken();
         $user = $token->getUser();
 
-        if ($user == "anon.") return;
+        if ($user == "anon.") {
+            return;
+        }
 
         //log the visit
-        $this->logger->debug("Logging visit for " . $user->getLogin());
+        $this->logger->debug("Logging visit for ".$user->getLogin());
 
         //currentvisit on user
         /** @var User $user */
@@ -44,5 +55,12 @@ class VisitLogService
         $stmt = $this->em->getConnection()->prepare($sql);
         $stmt->bindValue(":id", $user->getId());
         $stmt->execute();
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            AuthenticationEvents::AUTHENTICATION_SUCCESS => ['onSecurityAuthenticationSuccess'],
+        ];
     }
 }
