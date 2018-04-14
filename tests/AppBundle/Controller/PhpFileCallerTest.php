a9b4853feaccdcfb8dd1bb8d83734a4ea1e97f7f
Peter Dietrich <xosofox@gmail.com>
2018-04-14 11:19:35 +0200

    CURL test all php files

---
+++ tests/AppBundle/Controller/PhpFileCallerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Finder\Finder;
+
+class PhpFileCallerTest extends WebTestCase
+{
+    private $client;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+
+    }
+
+    private function call($fname)
+    {
+        $url = 'http://www.karoworld.de/'.$fname;
+        $ch = \curl_init($url);
+        \curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+        \curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+        \curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        \curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+        $output = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        $this->assertLessThan(
+            400,
+            $httpcode,
+            "Unexpected HTTP status code for GET /".$fname
+        );
+
+    }
+
+    public function testCallEachFile()
+    {
+        $finder = new Finder();
+        $files = $finder->files()->name('*.php')->depth(1)->in(__DIR__.'/../../../../')->sortByName();
+        /** @var \SplFileInfo $file */
+        foreach ($files as $file) {
+            $fname = $file->getFilename();
+            $this->call($fname);
+        }
+    }
+}
