410f8129419b50c14391491b85a08180776b2be7
Peter Dietrich <xosofox@gmail.com>
2018-06-17 23:49:02 +0200

    REAL game.html

---
+++ src/AppBundle/Controller/BackboneController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class BackboneController extends Controller
+
+{
+    /**
+     * @Route("/chat.html", name="backbone_chat")
+     * @Route("/dran.html", name="backbone_dran")
+     * @Route("/editor.html", name="backbone_editor")
+     * @Route("/newgame.html", name="backbone_newgame")
+     * @Route("/index.html", name="backbone_index")
+     * @Route("/game.html", name="backbone_game")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function htmlAction()
+    {
+        return new BinaryFileResponse(__DIR__.'/../../../web/index.html');
+    }
+
+    /**
+     * @Route("/game.html", name="backbone_game")
+     */
+    public function gameAction()
+    {
+        return new BinaryFileResponse(__DIR__.'/../../../web/game.html');
+    }
+}
