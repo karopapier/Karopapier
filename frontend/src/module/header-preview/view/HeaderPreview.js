f8c0d20730393d9409af9175bd2118c92224ca90
Peter Dietrich <xosofox@gmail.com>
2018-12-05 02:25:41 +0100

    PREVIEW limit to 20

--- frontend/src/module/header-preview/view/HeaderPreview.js
+++ frontend/src/module/header-preview/view/HeaderPreview.js
@@ -2,7 +2,16 @@ const Marionette = require('backbone.marionette');
 
 const HeaderGameThumbnail = require('./HeaderGameThumbnail');
 
-module.exports = Marionette.CollectionView.extend({
+module.exports = Marionette.NextCollectionView.extend({
     className: 'header-dran-preview',
     childView: HeaderGameThumbnail,
+
+    initialize() {
+        this.listenTo(this.collection, 'add remove', this.filter);
+    },
+
+    viewFilter(view, index) {
+        // only show 20 max
+        return index < 20;
+    },
 });
